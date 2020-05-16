d02ab1ca4f2aba03ec10cbea286684a86c02d1ef
LinHongJian123 <2460139692@qq.com>
2020-05-16 18:19:59 +0800

    用户名和头像同步

---
+++ admin/js/index.js
@@ -0,0 +1,14 @@
+$.ajax({
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/admin/user/login',
+    headers: {
+        'Authorization': localStorage.getItem('token')
+    },
+    success: function (res) {
+        if (res.code == 200) {
+            $('.user_info span i').text(res.data.nickname)
+            $('.user_info img').attr('str', res.data.userPic)
+            $('.user_center_link img').attr('src', res.data.userPic)
+        }
+    }
+})
