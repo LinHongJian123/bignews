3da519b822993b5713f3b03f91beb7134ee7e1c5
LinHongJian123 <2460139692@qq.com>
2020-05-16 18:09:18 +0800

    login登录和模态框实现

---
+++ admin/js/login.js
@@ -0,0 +1,33 @@
+$(function () {
+    $('.login_form').on('submit', function (e) {
+        e.preventDefault()
+
+        $.ajax({
+            type: 'post',
+            url: 'http://localhost:8080/api/v1/admin/user/login',
+            data: $(this).serialize(),
+            beforeSend: function () {
+                var flag = false;
+                $('.login_form input[name]').each(function (index, ele) {
+                    if ($.trim($(ele).val()) == '') {
+                        flag = true;
+                    }
+                })
+                if (flag) {
+                    $('.modal').modal('show')
+                    $('.modal-body p').text('输入的用户名或密码不能为空')
+                    return false;
+                }
+            },
+            success: function (res) {
+                $('.modal').modal('show')
+                $('.modal-body p').text(res.msg)
+                if (res.code == 200) {
+                    $('.modal').on('hidden.bs.modal', function (e) {
+                        window.location.href = './index.html'
+                    })
+                }
+            }
+        })
+    })
+})
