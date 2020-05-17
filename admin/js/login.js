8d83242cd6d219f1fdd8e0742c43c5d9489af718
LinHongJian123 <2460139692@qq.com>
2020-05-17 09:53:51 +0800

    实现登出退出与名字头像同步实时同步完成

--- admin/js/login.js
+++ admin/js/login.js
@@ -25,6 +25,7 @@ $(function () {
                 $('.modal-body p').text(res.msg)
                 if (res.code == 200) {
                     $('.modal').on('hidden.bs.modal', function (e) {
+                        localStorage.setItem('token', res.token)
                         window.location.href = './index.html'
                     })
                 }
