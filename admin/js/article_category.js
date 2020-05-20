f794557279407d9605540969862cf63a642be929
LinHongJian123 <2460139692@qq.com>
2020-05-20 11:44:58 +0800

    文章类别优化

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -2,7 +2,6 @@ $(function () {
     // 1. 发送请求获取数据，渲染页面
     // 1.1 发送ajax请求
     render()
-    // 封装渲染页面函数
     function render() {
         $.ajax({
             type: 'get',
@@ -19,59 +18,53 @@ $(function () {
         })
     }
 
-    // 2. 模态框的展示
-    // 2.1 给新增按钮注册事件 弹出模态框 
-    $('#xinzengfenlei').on('click', function () {
-        // 清空模态框
-        $('#myForm')[0].reset()  //重置表单
-        // 2.2 显示模态框
-        $('.addModal').modal('show')
 
-        // 2.3 修改提示标题
-        $('.addModal h4').text('新增文章分类')
-    })
+    // 2. 确定是哪个按钮弹出来的模态框
+    // 1. 由于当前我们是用的data-的方式来控制的模态框，而不是使用的.modal方法
+    // 2. 因为data-target data-toggle这两个属性我们都新增和编辑按钮都添加上了
+    // 3. 此时我们需要确定，到底是单击的哪个按钮来弹出来的模态框
+    // 4. e.relatedTarget 使用此属性来确定事件源头
+    $('#myModal').on('show.bs.modal', function (e) {
+        // console.log(e.relatedTarget );
+        // 2.1 判断当前按钮是新增还是编辑
+        if ($(e.relatedTarget).attr('id') == "xinzengfenlei") {
+            // alert('新增分类的操作')
+            //2.2 弹出模态框的时候，要将里面的数据进行重置
+            $('#myForm')[0].reset()
 
-
-
-    // 3. 编辑按钮
-    // 3.1 给编辑按钮注册事件 弹出模态框 要使用委托的方式业注册事件
-    // 因为当前的编辑按钮是通过模板创建出来的，相当于是动态创建出来的元素，直接注册事件，是不起作用的，所以需要使用委托的方式来注册事件
-    $('tbody').on('click', '.btn-edit', function () {
-        // 3.2 显示模态框
-        $('.addModal').modal('show')
-        // 2.3 修改提示标题
-        $('.addModal h4').text('更新文章分类')
-
-        // 2.4 发送ajax请求 
-        $.ajax({
-            type: 'get',
-            url: BigNew.category_search,
-            data: {
-                id: $(this).data('id')
-            },
-            success: function (res) {
-                // console.log(res)
-                if (res.code == 200) {
-                    // 将查询到的要编辑的数据先显示在模态框上
-                    $('#myForm input[name=id]').val(res.data[0].id)
-                    $('#myForm input[name=name]').val(res.data[0].name)
-                    $('#myForm input[name=slug]').val(res.data[0].slug)
+            // 修改提示标签
+            $('#myModal h4').text('新增文章分类')
+        } else {
+            $('#myModal h4').text('更新文章分类')
+            // alert('编辑分类操作')
+            // 2.3 获取当前按钮所在的id
+            // 2.4 向服务器发送请求获取对应的数据
+            $.ajax({
+                type: 'get',
+                url: BigNew.category_search,
+                data: {
+                    id: $(e.relatedTarget).data('id')
+                },
+                success: function (res) {
+                    // console.log(res)
+                    if (res.code == 200) {
+                        // 将查询到的要编辑的数据先显示在模态框上
+                        $('#myForm input[name=id]').val(res.data[0].id)
+                        $('#myForm input[name=name]').val(res.data[0].name)
+                        $('#myForm input[name=slug]').val(res.data[0].slug)
+                    }
                 }
-            }
-        })
-    })
+            }) // ajax
+        } // else
+    })// on
 
-
-    // 4. 新增或更新数据
-    // 4.1 给模态框的确定按钮注册事件
+    // 3. 新增或更新数据
     $('.addModal .btn-sure').on('click', function () {
         console.log(123)
-        // 4.2 获取隐藏域中的id
+        // 3.2 获取隐藏域中的id
         var id = $('#myForm input[name=id]').val()
-        console.log(id);
 
-
-        // 4.3 发送ajax请求
+        // 3.3 发送ajax请求
         $.ajax({
             type: 'post',
             url: id ? BigNew.category_edit : BigNew.category_add,
@@ -79,26 +72,24 @@ $(function () {
             success: function (res) {
                 // console.log(res)
                 if (res.code == 200 || res.code == 201) {
-                    // 4.4 隐藏模态框
+                    // 3.4 隐藏模态框
                     $('.addModal').modal('hide')
 
-                    // 4.5 刷新当前页面
+                    // 3.5 刷新当前页面
                     render()
                 }
             }
         })
     })
 
-    // 5. 删除数据
-    // 5.1 给删除按钮注册事件 
-    $('tbody').on('click', '.btn-del', function () {
-        // 5.2 弹出提示框 
-        $('.delModal').modal('show')
 
-        // 5.3 获取当前按钮所在的那条数据的id
-        window.categoryId = $(this).data('id')
+    // 4. 删除数据
+    // 4.1 . 注册弹出框事件，来获取事件源头
+    $('#delModal').on('show.bs.modal', function (e) {
+        window.categoryId = $(e.relatedTarget).data('id')
     })
 
+    // 4.2 给删除数据的模态框中的按钮注册事件
     // 5.4 给删除的模态框中的确定按钮注册事件
     $('.delModal .btn-sure').on('click', function () {
         // 5.5 向服务器端发送请求
@@ -122,12 +113,4 @@ $(function () {
         })
     })
 
-
-
-
-    // CRUD
-    // C create  增加 添加
-    // R read    查询 读取 获取 
-    // U update  更新 
-    // D delete  删除
-})
+}) //入口函数
--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -11,54 +11,49 @@ $(function () {
     })
 
 
-    // 2. 显示当前页面中的所有的文章数据
-    getDataByParams({
-        key: $('#key').val(),
-        type: $('#selCategory').val(),
-        state: $('#selStatus').val(),
-        page: 1,
-        perpage: 7
-    })
 
-    // 封装了一个根据不同参数获取数据的函数
-    function getDataByParams(obj) {
+    // 封装了一个根据不同条件来查询数据的函数
+    function getDataByParams(myPage, callback) {
         $.ajax({
             type: 'get',
             url: BigNew.article_query,
-            data: obj,
+            data: {
+                key: $('#key').val(),
+                type: $('#selCategory').val(),
+                state: $('#selStatus').val(),
+                page: myPage,
+                perpage: 7
+            },
             success: function (res) {
                 console.log(res)
                 if (res.code == 200) {
+                    // 2.2 渲染数据
                     var htmlStr = template('articleList', res.data)
                     $('tbody').html(htmlStr)
 
-                    // 服务器端的数据响应回来了之后, 启用分页功能
-                    pagination(res.data.totalPage)
+                    // 2.4  根据服务器响应回来的数据来判断是否显示控件
+                    if (res.data.totalPage == 0 && myPage == 1) {
+                        $('#pagination-demo').hide().next().show()
+                    } else if (res.data.totalPage != 0 && callback != null) {
+                        // 就说明是有数据响应回来的，应该要显示分页控件
+                        $('#pagination-demo').show().next().hide()
+                        // 2.3 实现函数的调用
+                        callback(res)
+                        // pagination(res)
+                    }
+
                 }
             }
         })
     }
+    // 2. 显示文章数据
+    // 2.1 一跳转到当前这个页面就要发送ajax请求
+    getDataByParams(1, pagination)
 
-    // 3. 给筛选按钮注册事件， 根据条件来查询文章数据渲染出来
-    // 3.1 给筛选按钮注册事件
-    $('#btnSearch').on('click', function (e) {
-        // 3.2 阻止默认行为
-        e.preventDefault()
-        // alert(123)
-        // 3.3 调用函数来获取数据
-        getDataByParams({
-            key: $('#key').val(),
-            type: $('#selCategory').val(),
-            state: $('#selStatus').val(),
-            page: 1,
-            perpage: 10
-        })
-    })
-
-    // 4. 实现分页功能
-    function pagination(totalPages, visiblePages) {
+    // 3. 分页功能的函数
+    function pagination(res, visiblePages) {
         $('#pagination-demo').twbsPagination({
-            totalPages: totalPages, // 总页数
+            totalPages: res.data.totalPage, // 总页数
             visiblePages: visiblePages || 7, // 可见最大上限页码值
             first: '首页',
             last: '最后一页',
@@ -68,14 +63,30 @@ $(function () {
             onPageClick: function (event, page) {
                 //  console.log(event,page);
                 // page是当前页码
-                getDataByParams({
-                    key: $('#key').val(),
-                    type: $('#selCategory').val(),
-                    state: $('#selStatus').val(),
-                    page: page,
-                    perpage: 7
-                })
+
+                // 调用方法，实现当前页码的数据渲染
+                getDataByParams(page, null)
             }
         })
     }
+
+    // 4. 给筛选按钮注册事件 根据新条件渲染页面
+    // 4.1 给筛选按钮注册事件
+    $('#btnSearch').on('click', function (e) {
+        // 4.2 阻止默认的请求行为
+        e.preventDefault()
+
+        // 4.3 发送请求获取数据
+
+        getDataByParams(1, function (res) {
+            // 更新分页控件的总页码
+            $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, 1)
+        })
+
+    })
 })
+
+/**
+ * 1. 当前项目不要纠结于某一行代码
+ * 2. 要听完整的思路
+ */
